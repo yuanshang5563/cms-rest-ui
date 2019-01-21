bee7bec1ecd5036d0549d91c62b5dff58021a0cc
yuanshang5563 <yuanshang5563@163.com>
2019-01-21 13:48:34 +0800

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import vuex from 'vuex'
+
+Vue.use(vuex);
+
+import app from './modules/app'
+import iframe from './modules/iframe'
+import user from './modules/user'
+import menu from './modules/menu'
+
+const store = new vuex.Store({
+    modules: {
+        app: app,
+        iframe: iframe,
+        user: user,
+        menu: menu
+    }
+})
+
+export default store
